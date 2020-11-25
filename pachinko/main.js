7436054aee86e25d19b55524067e877a5d1d77e5
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-25 15:31:13 -0500

    Include challenge

---
+++ pachinko/main.js
@@ -0,0 +1,5 @@
+//Build a simple slot machine with minimum 5 items per reel and 3 reels - user should be able to bet min or max and have their total update aka how much money they have won or lost. Min bet does $5 and Max bet does $50. They should start with $1000. Matches of the three wheels = a win just like a normal slot machine and they win 10x their bet
+
+function pachinko(){
+  
+}
